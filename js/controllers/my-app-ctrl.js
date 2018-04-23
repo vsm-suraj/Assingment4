4f52b038983d535425d9ba27fb236f342c220db1
SURAJ <suraj@getvsm.com>
2018-04-23 15:33:25 +0530

    Assignment 4

--- js/controllers/my-app-ctrl.js
+++ js/controllers/my-app-ctrl.js
@@ -1,7 +1,6 @@
 myApp.controller('myAppCtrl', function ($scope, emotionDataService, instantaniusDataService) {
     emotionDataService.data().then( function (data) {
         $scope.emotionData = data;
-        console.log(data);
     });
 
     instantaniusDataService.data().then(function (data) {
--- js/my-app.js
+++ js/my-app.js
@@ -1,1 +1,9 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngResource']);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider.when('/annotations', {
+        templateUrl: 'templates/annotations.html'
+    }).when('/tone-analysis', {
+        templateUrl: 'templates/tone-analysis.html'
+    });
+}); 
